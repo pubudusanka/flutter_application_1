eda32417363656cff0eb878c3d2cd7d4ecd492e7
Pubudu Sanka <pubudusanka79@gmail.com>
2025-01-23 12:24:21 +0530

    Create book_card.dart

---
+++ lib/widgets/book_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class BookCard extends StatelessWidget {
+  final String coverImagePath;
+  final String title;
+  final String author;
+  final double price;
+
+  const BookCard({
+    Key? key,
+    required this.coverImagePath,
+    required this.title,
+    required this.author,
+    required this.price,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            // Book Cover Image
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+              ),
+              child: Image.asset(
+                coverImagePath,
+                width: 100,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 16.0),
+
+            // Book Details
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      'Author: $author',
+                      style: const TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      'Price: Rs. $price',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
