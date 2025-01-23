fb243211299e35990c5bbec65a3dbb10abcf665a
Pubudu Sanka <pubudusanka79@gmail.com>
2025-01-23 14:45:10 +0530

    check the errors and fixed it

--- lib/screens/book_list_screen.dart
+++ lib/screens/book_list_screen.dart
@@ -2,35 +2,23 @@ import 'package:flutter/material.dart';
 import '../widgets/book_card.dart';
 
 class BookListScreen extends StatelessWidget {
-  const BookListScreen({super.key});
+  const BookListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sarasavi Bookstore'),
+        title: const Text('Ceylon Bookstore'),
         backgroundColor: Colors.blueGrey,
       ),
       body: SafeArea(
         child: ListView(
           children: const [
             BookCard(
-              coverImagePath: 'assets/images/rich-dad-poor-dad.png',
+              coverImagePath: 'lib/assets/images/rich_dad_poor_dad.png',
               title: 'Rich Dad, Poor Dad',
               author: 'Robert T. Kiyosaki',
               price: 1250.00,
-            ),
-            BookCard(
-              coverImagePath: 'assets/images/the-alchemist.png',
-              title: 'The Alchemist',
-              author: 'Paulo Coelho',
-              price: 1800.00,
-            ),
-            BookCard(
-              coverImagePath: 'assets/images/men-are-from-mars.png',
-              title: 'Men Are From Mars',
-              author: 'John Gray',
-              price: 1000.00,
             ),
           ],
         ),
