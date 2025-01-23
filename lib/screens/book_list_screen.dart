03c67e69a6f7886694dfc4fe29f4aa3980c47f3d
Pubudu Sanka <pubudusanka79@gmail.com>
2025-01-23 12:22:32 +0530

    Create book_list_screen.dart

---
+++ lib/screens/book_list_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../widgets/book_card.dart';
+
+class BookListScreen extends StatelessWidget {
+  const BookListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ceylon Bookstore'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: const [
+            BookCard(
+              coverImagePath: 'assets/images/rich-dad-poor-dad.jpg',
+              title: 'Rich Dad, Poor Dad',
+              author: 'Robert T. Kiyosaki',
+              price: 1250.00,
+            ),
+            BookCard(
+              coverImagePath: 'assets/images/the-alchemist.jpg',
+              title: 'The Alchemist',
+              author: 'Paulo Coelho',
+              price: 1800.00,
+            ),
+            BookCard(
+              coverImagePath: 'assets/images/men-are-from-mars.jpg',
+              title: 'Men Are From Mars',
+              author: 'John Gray',
+              price: 1000.00,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
